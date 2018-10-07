9cd656420188b3f4b3efee5f069e8559d5208908
KimJunHyeong <jhkim4685@gmail.com>
2018-10-08 00:05:23 +0900

    geestbook

--- java/FastCampus/MiniPJT/guestbook/src/main/java/my/examples/guestbook/dao/GuestbookDao.java
+++ java/FastCampus/MiniPJT/guestbook/src/main/java/my/examples/guestbook/dao/GuestbookDao.java
@@ -3,8 +3,11 @@ package my.examples.guestbook.dao;
 import my.examples.guestbook.servlet.Guestbook;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +22,21 @@ public class GuestbookDao {
         PreparedStatement ps = null;
         ResultSet rs = null;
         try{
+            conn = DbUtil.connect(dbUrl, dbId, dbPassword);
+            String sql = "select id, name, content, regdate from guestbook order by id desc";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
 
+            while(rs.next()){
+                Guestbook guestbook = new Guestbook();
+                guestbook.setId(rs.getLong(1));
+                guestbook.setName(rs.getString(2));
+                guestbook.setContent(rs.getString(3));
+                Date dbDate = rs.getDate(4);    // java.sql.Date
+                LocalDateTime ldt = dbDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+                guestbook.setRegate(ldt);
+                list.add(guestbook);
+            }
         }catch (Exception ex){
             ex.printStackTrace();
         }finally{
