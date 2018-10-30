6440a9421c5acdd35416e0e57dad1ab1271e0b90
KimJunHyeong <jhkim4685@gmail.com>
2018-10-30 20:56:08 +0900

    오류 수정

--- fastcampus_JWPM/spring/daoexam/src/main/java/examples/daoexam/config/WebMvcContextConfiguration.java
+++ fastcampus_JWPM/spring/daoexam/src/main/java/examples/daoexam/config/WebMvcContextConfiguration.java
@@ -9,27 +9,31 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@Configuration      // java Config 임을 알려줌
-@EnableWebMvc       // 자동으로 Spring MVC에 대한 설정
+// /doc/Readme2.txt 참조
+@Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = { "examples.daoexam.controller"})
-public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {   // 필요한 기능이 있다면 Bean을 생성하거나 메소드를 오버라이딩하여 설정
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
         registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
-        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/images/**").addResourceLocations("/img/").setCachePeriod(31556926);
         registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
     }
+
     // default servlet handler를 사용하게 합니다.
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
-    //    @Override
+
+//    @Override
 //    public void addViewControllers(final ViewControllerRegistry registry) {
 //        System.out.println("addViewControllers가 호출됩니다. ");
 //        registry.addViewController("/").setViewName("main");
 //    }
+
     @Bean
     public InternalResourceViewResolver getInternalResourceViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
--- fastcampus_JWPM/spring/daoexam/src/main/java/examples/daoexam/controller/MainController.java
+++ fastcampus_JWPM/spring/daoexam/src/main/java/examples/daoexam/controller/MainController.java
@@ -1,7 +1,9 @@
 package examples.daoexam.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MainController {
     public MainController() {
         System.out.println("MainController()!");
