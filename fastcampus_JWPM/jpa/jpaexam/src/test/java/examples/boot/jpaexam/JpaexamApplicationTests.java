22f0fcbbbc4002f9438f25ab564596a2c81b8ddd
KimJunHyeong <jhkim4685@gmail.com>
2018-11-13 09:20:31 +0900

    no message

--- fastcampus_JWPM/jpa/jpaexam/src/test/java/examples/boot/jpaexam/JpaexamApplicationTests.java
+++ fastcampus_JWPM/jpa/jpaexam/src/test/java/examples/boot/jpaexam/JpaexamApplicationTests.java
@@ -1,16 +1,23 @@
 package examples.boot.jpaexam;
 
+import examples.boot.jpaexam.service.BoardService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JpaexamApplicationTests {
-
+	@Autowired
+	BoardService boardService;
 	@Test
 	public void contextLoads() {
+		// 해당 메소드가 실행될 때 문제가 없다는 것은 모든 설정이 잘되어 있다.
 	}
-
+	@Test
+	public void saveBoard() throws Exception{
+		boardService.test();
+	}
 }
