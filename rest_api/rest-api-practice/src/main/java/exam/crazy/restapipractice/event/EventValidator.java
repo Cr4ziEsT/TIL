2af5f0a376ca3e8d7f670729f6c3d2854e945fc5
Kim-JunHyeong <jhkim4685@gmail.com>
2019-01-26 23:24:42 +0900

    Add : EventValidate Class, validate wrong data

---
+++ rest_api/rest-api-practice/src/main/java/exam/crazy/restapipractice/event/EventValidator.java
@@ -0,0 +1,29 @@
+package exam.crazy.restapipractice.event;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors) {
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
+            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        // 이벤트가 끝나는 날짜가 이벤트의 시작일, 접수종료일, 접수시작일 이전이면 안 됨
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+        endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+        endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "worngValue","EndEventDateTime is wrong");
+        }
+
+        // TODO BeginEventDateTime
+        // TODO CloseEnrollmentDateTime
+    }
+}
