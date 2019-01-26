f6d1304760e48e1f60281f10980fa5dd3bfc8016
KimJunHyeong <jhkim4685@gmail.com>
2019-01-26 19:11:01 +0900

    Init : rest-api-parctice 프로젝트 생성

---
+++ rest_api/rest-api-practice/src/main/java/exam/crazy/restapipractice/RestApiPracticeApplication.java
@@ -0,0 +1,14 @@
+package exam.crazy.restapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiPracticeApplication.class, args);
+    }
+
+}
+
