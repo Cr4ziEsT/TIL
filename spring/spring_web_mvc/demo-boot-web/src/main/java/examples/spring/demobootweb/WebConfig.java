552bbb67e3766ece0e8c962c1d8cfa283e53ebc0
Kim-JunHyeong <jhkim4685@gmail.com>
2019-02-03 01:41:58 +0900

    ResourceHandlerRegistry

--- spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
+++ spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
@@ -2,16 +2,15 @@ package examples.spring.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -21,5 +20,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(-1);
         // `order`를 설정하지 않으면 적은 순서대로 실행이 됨
         // `order`의 값은 낮을수록 우선순위가 높다.
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/", "file:/Users/keesun/files/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
+//                .resourceChain(true)    // 운영중이라면 true 개발중이라면 false 가 편할 것
+                // Transformer / Resolver
+                // Resolver : 어떤 요청에 해당하는 리소스를 찾는 것
+                // Transformer : 응답으로 내보낼 그 리소스를 변경하는 방법
+        ;
     }
 }
--- spring/spring_web_mvc/demo-boot-web/src/test/java/examples/spring/demobootweb/SampleControllerTest.java
+++ spring/spring_web_mvc/demo-boot-web/src/test/java/examples/spring/demobootweb/SampleControllerTest.java
@@ -1,17 +1,21 @@
 package examples.spring.demobootweb;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,6 +38,25 @@ public class SampleControllerTest {
                     .param("id", savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello keesun"));
+    }
+
+    @Test
+    public void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello index")))
+        ;
+    }
+
+    @Test
+    public void helloStatic2() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello Mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL))
+        ;
     }
 
 }
