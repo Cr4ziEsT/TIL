5a73a0eb18cfdce5236e40f943a51b18c63464d9
Kim-JunHyeong <jhkim4685@gmail.com>
2019-02-03 00:48:09 +0900

    Domain Class Converter 설정

---
+++ spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/PersonRepository.java
@@ -0,0 +1,7 @@
+package examples.spring.demobootweb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
--- spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
+++ spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
@@ -7,11 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addFormatters(FormatterRegistry registry) {
-//        // 포매터와 컨버터를 추가할 수 있다.
-//        // 컨버터는 포매터보다 좀 더 일반적인 용도로 문자열이 아닌 일반적인 자바 객체에서 또 다른 일반적인 자바 객체로 변환할 수 있는 인터페이스
-////        registry.addConverter();
-//        registry.addFormatter(new PersonFormatter());
-//    }
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+    }
 }
