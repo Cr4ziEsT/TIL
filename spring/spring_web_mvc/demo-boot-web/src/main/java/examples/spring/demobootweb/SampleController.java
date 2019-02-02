3d8dd09fb7e73152b9333559416136f5dbdd7033
Kim-JunHyeong <jhkim4685@gmail.com>
2019-02-03 01:22:48 +0900

    HandlerInterceptor 구현 및 등록

--- spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/SampleController.java
+++ spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/SampleController.java
@@ -1,12 +1,20 @@
 package examples.spring.demobootweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+    // 핸들러 인터셉터
+    // preHandle 1
+    // preHandle 2
+    // 요청 처리
+    // postHandler 2
+    // postHandler 1
+    // 뷰 랜더링
+    // afterCompletion 2
+    // afterCompletion 1
 
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
--- spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
+++ spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
@@ -2,6 +2,7 @@ package examples.spring.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +11,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
 
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi/**")
+                .order(-1);
+        // `order`를 설정하지 않으면 적은 순서대로 실행이 됨
+        // `order`의 값은 낮을수록 우선순위가 높다.
     }
 }
