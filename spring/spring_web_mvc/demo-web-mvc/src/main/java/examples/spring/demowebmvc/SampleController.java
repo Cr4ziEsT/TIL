8b74f0e1c70a55f1927fdb0bbeacb7fe7a8463a5
KimJunHyeong <jhkim4685@gmail.com>
2019-02-03 22:22:47 +0900

    Headers and Request Parameter

--- spring/spring_web_mvc/demo-web-mvc/src/main/java/examples/spring/demowebmvc/SampleController.java
+++ spring/spring_web_mvc/demo-web-mvc/src/main/java/examples/spring/demowebmvc/SampleController.java
@@ -1,5 +1,6 @@
 package examples.spring.demowebmvc;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 
-    // Json 요청만을 처리하는 핸들러
-    @RequestMapping(
-            value = "/hello",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.TEXT_PLAIN_VALUE)
+//    @RequestMapping(value = "/hello", headers = "!" + HttpHeaders.FROM)
+    @RequestMapping(value = "/hello", params = "name")
     @ResponseBody
     public String hello() {
         return "hello";
