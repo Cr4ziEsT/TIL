6bfd817b13a39bbf0c12d029d516455c76bdb719
KimJunHyeong <jhkim4685@gmail.com>
2019-02-03 12:59:24 +0900

    의존성 추가

---
+++ spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/DemoBootWebApplication.java
@@ -0,0 +1,14 @@
+package examples.spring.demobootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBootWebApplication.class, args);
+    }
+
+}
+
