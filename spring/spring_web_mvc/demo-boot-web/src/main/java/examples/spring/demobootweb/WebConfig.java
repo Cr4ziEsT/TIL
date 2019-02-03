b45364e5193f7a835b13f96831b2f46fb8f1ce21
KimJunHyeong <jhkim4685@gmail.com>
2019-02-03 14:07:18 +0900

    MVC 설정 마무리

--- spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
+++ spring/spring_web_mvc/demo-boot-web/src/main/java/examples/spring/demobootweb/WebConfig.java
@@ -2,19 +2,28 @@ package examples.spring.demobootweb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.http.CacheControl;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /* 스프링 설정 파일 작성하는 법
+    1. 스프링 부트를 사용하는 경우에는 `application.properties` 로 작성할 수 있는지를 먼저 확인
+    2. `WebMvcConfigurer`를 구현하여 확장
+    3. 위의 2가지 방법으로도 안되면 `@Bean`으로 MVC 구성 요소 직접 등록
+    */
+
+    // 간단한 핸들러를 작성할 수 있다.
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hi").setViewName("hi");
+    }
 
     @Bean
     public Jaxb2Marshaller jaxb2Marshaller() {
@@ -54,4 +63,5 @@ public class WebConfig implements WebMvcConfigurer {
 //    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 //        // 기본 컨버터에 추가로 메세지 컨버터를 추가하고 싶을 때 사용, 하지만 대부분 사용하지 않음
 //    }
+
 }
