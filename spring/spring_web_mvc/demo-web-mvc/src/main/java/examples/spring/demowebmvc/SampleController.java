a49a5110af879787a8aa4d59793d1bfb70c71c00
KimJunHyeong <jhkim4685@gmail.com>
2019-02-03 21:59:36 +0900

    Consumes and Produces

--- spring/spring_web_mvc/demo-web-mvc/src/main/java/examples/spring/demowebmvc/SampleController.java
+++ spring/spring_web_mvc/demo-web-mvc/src/main/java/examples/spring/demowebmvc/SampleController.java
@@ -1,19 +1,20 @@
 package examples.spring.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
 public class SampleController {
 
-//    @GetMapping({"/hello", "/hi"})
-    @RequestMapping("/junhyeong")
+    // Json 요청만을 처리하는 핸들러
+    @RequestMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
-    public String helloJunhyeong() {
-        return "hello junhyeong";
+    public String hello() {
+        return "hello";
     }
 }
