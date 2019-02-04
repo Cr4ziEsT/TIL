4548fcfa372186aff8c3a6a2b9071908d83716bb
KimJunHyeong <jhkim4685@gmail.com>
2019-02-04 13:35:39 +0900

    @SessionAttribute

--- spring/spring_web_mvc/demo-web-mvc/src/main/java/examples/spring/demowebmvc/SampleController.java
+++ spring/spring_web_mvc/demo-web-mvc/src/main/java/examples/spring/demowebmvc/SampleController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,7 +49,9 @@ public class SampleController {
     }
 
     @GetMapping("/events/list")
-    public String getEvents(Model model) {
+    public String getEvents(Model model,
+                            @SessionAttribute LocalDateTime visitTime) {
+        System.out.println(visitTime);
         List<Event> eventList = new ArrayList<>();
         Event event = new Event();
         event.setLimit(10);
